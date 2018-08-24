d7dc9900fba7e0f0d4eed8074381962d6f48df7e
jl <jonathan.arthur.lock@gmail.com>
2018-08-24 18:30:04 +0200

    python based version functional
    just style changes needed for golive state

--- rijk/rijk.py
+++ rijk/rijk.py
@@ -1,15 +1,24 @@
 from flask import Flask, render_template
+import json, requests
+from random import randint
 
 app = Flask(__name__)
 
 
+@app.route('/', methods=['GET'])
+def random():
+	APIKEY ='9UogSlO5'
+	P = str(randint(1,10000))
+	r = requests.get("https://www.rijksmuseum.nl/api/en/collection/?key=" + APIKEY + "&format=json&imgonly=True&ps=1&p=" + P)
+	rjson = json.loads(r.text)
 
+	artimg = rjson['artObjects'][0]['webImage']['url']
+	arttitle = rjson['artObjects'][0]['title']
+	artlongtitle = rjson['artObjects'][0]['longTitle']
+	artist = rjson['artObjects'][0]['principalOrFirstMaker']
+	artlink = rjson['artObjects'][0]['links']['web']
 
-@app.route('/')
-def index():
-    
-    return render_template('index.html')
-
-
+	return render_template('index.html', artimg=artimg, arttitle=arttitle, artlongtitle=artlongtitle, artist=artist, artlink=artlink)
+	
 if __name__ == '__main__':
     app.run()
